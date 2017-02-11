fba3259468d9418e81a605bfbb1db7ab2aace0a0
Brandon Hinds <brandon.hinds@mymona.uwi.edu>
2017-02-11 21:35:16 +0000

    Done

--- app/views.py
+++ app/views.py
@@ -5,8 +5,10 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+import smtplib
 from app import app
-from flask import render_template, request, redirect, url_for
+from forms import MessageForm
+from flask import render_template, request, redirect, url_for, flash
 
 
 ###
@@ -23,6 +25,27 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+@app.route('/contact', methods=["POST","GET"])
+def contact():
+    """Render contact page"""
+    
+    contact_form = MessageForm()
+
+    if request.method == 'POST':
+        if contact_form.validate_on_submit():
+            # Get validated data from form
+            name = contact_form["name"]
+            email = contact_form["email"]
+            subject = contact_form["subject"]
+            message = contact_form["message"]
+            
+            send_email(name, email, subject, message)
+            
+            flash('Message Sent')
+            return redirect(url_for('home'))
+    
+    return render_template('contact.html', form=contact_form)
 
 
 ###
@@ -51,6 +74,24 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+   
+    
+def send_email(from_name, from_email, subject, msg):
+
+    to_name = "Brandon"
+    to_addr = '@gmail.com'
+    message = """From: {} <{}> To: {} <{}>Subject: {} {}"""
+
+    message_to_send = message.format(from_name, from_email, to_name,to_addr, subject, msg)
+
+    username = '@gmail.com'
+    password = ''
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_addr, message_to_send)
+    server.quit()
 
 
 if __name__ == '__main__':
